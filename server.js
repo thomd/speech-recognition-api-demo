00a66df4ec0afbd3149f9aca03b674999a0b7010
Thomas Duerr <thomduerr@gmail.com>
2016-06-26 21:35:04 +0200

    accept language

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import expressLogging from 'express-logging'
 import logger from 'logops'
+import accepts from 'accepts'
 
 const app = express()
 app.set('views', './views')
 app.set('view engine', 'jade')
-//app.use(express.static('./public'))
+app.use(express.static('./public'))
 app.use(expressLogging(logger))
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', (request, response) => {
+  let languages = accepts(request).languages()
+  let language = languages[0]
+  response.render('index', {language})
 })
 app.listen(3000)
 
